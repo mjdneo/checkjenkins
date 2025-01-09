068606d19d253c5143200486503cf62407ad045a
neojarvis <techadmin@iamneo.ai>
2025-01-09 14:32:37 +0000

    Auto-save-commit-09/01/2025--20:02:37

--- demobdd/src/test/java/com/jio/runner/TestRun.java
+++ demobdd/src/test/java/com/jio/runner/TestRun.java
@@ -2,7 +2,18 @@ package com.jio.runner;
 
 import org.junit.runner.RunWith;
 
-@RunWith(Cu )
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+   // features = "./features/login.feature" , glue = "com.jio.stepdefinitions"
+   features = "./features/test.feature" , glue = "com.jio.stepdefinitions"
+)
+
+
+
 public class TestRun {
 
 }
---
+++ demobdd/src/test/java/com/jio/stepdefinitions/LoginStepDefinitions.java
@@ -0,0 +1,47 @@
+package com.jio.stepdefinitions;
+
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+
+    WebDriver driver;
+
+    @Given("User opened the browser and launched jiomart application")
+    public void user_opened_the_browser_and_launched_jiomart_application() {
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+            driver.get("https://www.jiomart.com");
+            Thread.sleep(4000);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
+    @When("User clicks on {string} button")
+    public void user_clicks_on_button(String string) {
+
+    }
+
+    @And("User enters the valid mobile number in the mobile number field")
+    public void user_enters_the_valid_mobile_number_in_the_mobile_number_field() {
+
+    }
+
+    @Then("User will be prompted to enter the valid otp")
+    public void user_will_be_prompted_to_enter_the_valid_otp() {
+        System.out.println(driver.getCurrentUrl());
+        driver.quit();
+    }
+
+}
