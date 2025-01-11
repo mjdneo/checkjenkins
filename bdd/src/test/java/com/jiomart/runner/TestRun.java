f9d8cfc4a9254fe3f52609c2bda7a98ca5b32d87
neojarvis <techadmin@iamneo.ai>
2025-01-11 05:17:29 +0000

    Auto-save-commit-11/01/2025--10:47:27

---
+++ bdd/src/test/java/com/jiomart/runner/TestRun.java
@@ -0,0 +1,16 @@
+package com.jiomart.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+    features = "./features/sigin.feature"
+)
+
+public class TestRun {
+
+}
