4c6aa49dc1c9889d2e4afd725e6f41a836eca789
neojarvis <techadmin@iamneo.ai>
2025-01-11 09:20:21 +0000

    Auto-save-commit-11/01/2025--14:50:20

--- bdd/features/jiomart.feature
+++ bdd/features/jiomart.feature
@@ -1,15 +1,17 @@
 Feature: Jiomart Feature
 
-    Scenario: search
-        Given User open the application
-        When User click on the advance search icon, the search bar opens
-        And User input two search values "Bread" and "Butter" inside search bar
-        And User clicks on searchAll button
-        Then the search results page should display two search result products. 
+    Background: Open Browser
+    Given Open the Browser
     
-    Scenario: signin
-        Given User opened the Jiomart web application.
-        When the user clicks on sign in, they are redirected to the sign-in module.
-        And the user input the valid "9988776655" mobile number.
-        And the user clicks on the continue button.
-        Then the user should be prompted to enter the valid OTP.
+    Scenario: sigin
+            Given User opened the Jiomart web application.
+            When the user clicks on sign in, they are redirected to the sign-in module.
+            And the user input the valid "9988776655" mobile number.
+            And the user clicks on the continue button.
+            Then the user should be prompted to enter the valid OTP.
+    Scenario: search
+            Given User open the application
+            When User click on the advance search icon, the search bar opens
+            And User input two search values "Bread" and "Butter" inside search bar
+            And User clicks on searchAll button
+            Then the search results page should display two search result products. 
--- bdd/src/test/java/com/jiomart/stepdefinitions/SigninStepDefinition.java
+++ bdd/src/test/java/com/jiomart/stepdefinitions/SigninStepDefinition.java
@@ -1,10 +1,64 @@
 package com.jiomart.stepdefinitions;
 
-
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class SigninStepDefinition {
 
-    
-   
+    @Given("Open the Browser")
+    public void open_the_browser() {
+        
+    }
+
+    @Given("User open the application")
+    public void user_open_the_application() {
+        System.out.println("Search Functionality Started");
+    }
+
+    @When("User click on the advance search icon, the search bar opens")
+    public void user_click_on_the_advance_search_icon_the_search_bar_opens() {
+
+    }
+
+    @When("User input two search values {string} and {string} inside search bar")
+    public void user_input_two_search_values_and_inside_search_bar(String string, String string2) {
+
+    }
+
+    @When("User clicks on searchAll button")
+    public void user_clicks_on_search_all_button() {
+
+    }
+
+    @Then("the search results page should display two search result products.")
+    public void the_search_results_page_should_display_two_search_result_products() {
+        System.out.println("Search Functionality Completed");
+    }
+
+    @Given("User opened the Jiomart web application.")
+    public void user_opened_the_jiomart_web_application() {
+        System.out.println("Sigin Functionality Started");
+    }
+
+    @When("the user clicks on sign in, they are redirected to the sign-in module.")
+    public void the_user_clicks_on_sign_in_they_are_redirected_to_the_sign_in_module() {
+
+    }
+
+    @When("the user input the valid {string} mobile number.")
+    public void the_user_input_the_valid_mobile_number(String string) {
+
+    }
+
+    @When("the user clicks on the continue button.")
+    public void the_user_clicks_on_the_continue_button() {
+
+    }
+
+    @Then("the user should be prompted to enter the valid OTP.")
+    public void the_user_should_be_prompted_to_enter_the_valid_otp() {
+        System.out.println("Sigin Functionality Completed");
+    }
 
 }
