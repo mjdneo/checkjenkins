e4f1527762506e1d98f981112ca56aeb07340a80
neojarvis <techadmin@iamneo.ai>
2025-01-13 14:20:32 +0000

    Manual-save-commit-13/01/2025--19:50:31

--- rwd/src/test/java/com/rwd/TestRWD.java
+++ rwd/src/test/java/com/rwd/TestRWD.java
@@ -19,8 +19,8 @@ public class TestRWD {
     @Test (priority = 1)
     public void desktopView() throws MalformedURLException, InterruptedException{
         driver = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
-        Dimension desktop = new Dimension(1440, 305);
-        driver.manage().window().setSize(desktop);
+        Dimension desktopview = new Dimension(1920, 1080);
+        driver.manage().window().setSize(desktopview);
         driver.get("https://ltimindtree.iamneo.ai/");
         Thread.sleep(4000);
         driver.quit();
